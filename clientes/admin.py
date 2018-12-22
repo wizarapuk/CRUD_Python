dd1118a26dda540069fdde0d2ea9e106cae6bd7b
Thierre <thierre_12@hotmail.com>
2018-12-22 21:53:35 -0200

    'Alteração no home'

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, documento, Venda, Produto
 
 admin.site.register(Person)
+admin.site.register(documento)
+admin.site.register(Venda)
+admin.site.register(Produto)
 # Register your models here.
--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import home, my_logout
+from clientes.views import person_list
 from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', person_list, name='person_list'),
     path('logout', my_logout, name='logout'),
 
 ]
